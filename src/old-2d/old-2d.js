a101ebb4bed3e1a5f515914bed3463a83ebdb886
kungfooman <lama12345@gmail.com>
2023-12-28 17:20:05 +0100

    simplify/shorten code

--- src/old-2d/old-2d.js
+++ src/old-2d/old-2d.js
@@ -15,21 +15,19 @@ export function old2d(regl) {
    * @param {number} alpha 
    */
   function render(canvas, segments, alpha) {
-    const points = generateSamplePointsInterleaved(canvas.width, canvas.height);
-    const projection = mat4.ortho(mat4.create(), -canvas.width / 2, canvas.width / 2, -canvas.height / 2, canvas.height / 2, 0, -1);
-    const viewport = { x: 0, y: 0, width: canvas.width, height: canvas.height };
-    const scaleX = 0.45 * Math.sin(performance.now() * 0.002) + 0.75;
-    const scaleY = Math.sin(performance.now() * 0.0003);
-    const scaledData = [];
-    for (const point of points) {
-      scaledData.push([point[0] * scaleX, point[1] * scaleY]);
-    }
+    const {width, height} = canvas;
+    const points     = generateSamplePointsInterleaved(width, height);
+    const projection = mat4.ortho(mat4.create(), -width / 2, width / 2, -height / 2, height / 2, 0, -1);
+    const viewport   = {x: 0, y: 0, width, height};
+    const scaleX     = 0.45 * Math.sin(performance.now() * 0.002) + 0.75;
+    const scaleY     = Math.sin(performance.now() * 0.0003);
+    const scaledData = points.map(([x, y]) => [x * scaleX, y * scaleY]);
     regl.clear({ color: [0, 0, 0, 0] });
     const command = segments ? interleavedStrip : roundRound;
     command({
       points: pointsBuffer(scaledData),
       segments: points.length - 1,
-      width: canvas.width * 0.05,
+      width: width * 0.05,
       color: [0.25, 0.25, 0.25, alpha],
       projection,
       viewport,
